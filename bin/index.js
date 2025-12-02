c9a6f12972f9ba33202bed0d4a2ad4e673a3eddb
federicoferrulli <federico.ferrulli01@gmail.com>
2025-12-03 00:44:09 +0100

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 import { program } from "commander";
+import { readFile, writeFile } from "fs/promises";
 
-console.log(process.cwd());
+const directory = process.cwd();
+const fileName = process.argv.at(2);
+
+if (!fileName) {
+    console.log("Insert an example file!");
+    process.exit();
+}
+
+let json = null;
+try {
+    let file = null;
+    file = await readFile(`${directory}/${fileName}.json`, "utf-8");
+    json = JSON.parse(file);
+} catch (e) {
+    console.error(e);
+    process.exit();
+}
+
+console.log(json);
+process.exit();
